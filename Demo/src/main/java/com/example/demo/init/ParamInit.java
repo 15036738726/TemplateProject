9d76a781a0a93a89c6553673f930a17444890057
15036738726 <15036738726@163.com>
2023-01-17 10:47:53 +0800

    第一提交测试

---
+++ Demo/src/main/java/com/example/demo/init/ParamInit.java
@@ -0,0 +1,36 @@
+package com.example.demo.init;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 服务器启动时,参数配置读取
+ */
+
+@Component
+public class ParamInit implements ApplicationRunner {
+
+    @Autowired
+    private Environment environment;
+
+    public static Map<String, String> CACHE = new HashMap<>();
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        CACHE.put("driver-class-name",environment.getProperty("spring.datasource.driver-class-name"));
+        CACHE.put("url",environment.getProperty("spring.datasource.url"));
+        CACHE.put("username",environment.getProperty("spring.datasource.username"));
+        CACHE.put("password",environment.getProperty("spring.password"));
+    }
+
+    public Map<String,String> getCACHE(){
+        return CACHE;
+    }
+}
---
+++ Demo/src/main/java/com/example/demo/test/UserController.java
@@ -0,0 +1,105 @@
+package com.example.demo.test;
+
+import com.example.demo.init.ParamInit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private ParamInit paramInit;
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public String queryUser(){
+        return "哈喽";
+    }
+
+
+    @RequestMapping("shgh")
+    @ResponseBody
+    public Map queryShgh(){
+        Connection connection = null;
+        PreparedStatement ps = null;
+        Map<String, String> map = null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "bxfundacc", "bxfundacc");
+            ps = connection.prepareStatement("SELECT * from A001HzJkHZ");
+            ResultSet result = ps.executeQuery();
+            int i = 1;
+            map = new HashMap<>();
+            while (result.next()){
+                Map<String, String> tempMap = new HashMap<>();
+                String fdate = result.getString("FDATE");
+                String fzqdm = result.getString("FZQDM");
+                tempMap.put("fadate",fdate);
+                tempMap.put("fzqdm",fzqdm);
+                map.put("第"+ i++ +"条数据" ,tempMap.toString());
+            }
+            result.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return map;
+    }
+
+
+    @RequestMapping("shgh2")
+    @ResponseBody
+    public Map queryShgh2(){
+        Connection connection = null;
+        PreparedStatement ps = null;
+        Map<String, String> map = null;
+        try {
+            Class.forName(paramInit.getCACHE().get("driver-class-name"));
+            connection = DriverManager.getConnection(paramInit.getCACHE().get("url"), paramInit.getCACHE().get("username"),paramInit.getCACHE().get("password"));
+            ps = connection.prepareStatement("SELECT * from A001HzJkHZ");
+            ResultSet result = ps.executeQuery();
+            int i = 1;
+            map = new HashMap<>();
+            while (result.next()){
+                Map<String, String> tempMap = new HashMap<>();
+                String fdate = result.getString("FDATE");
+                String fzqdm = result.getString("FZQDM");
+                tempMap.put("fadate",fdate);
+                tempMap.put("fzqdm",fzqdm);
+                map.put("第"+ i++ +"条数据" ,tempMap.toString());
+            }
+            result.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return map;
+    }
+
+}
